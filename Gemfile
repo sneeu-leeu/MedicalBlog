8deddf352e69088f350d51b28fab6d54d93df9f3
sneeu-leeu <sa070197@gmail.com>
2022-05-12 09:29:33 +0200

    create user specs

--- Gemfile
+++ Gemfile
@@ -53,9 +53,15 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
-  %w[rspec-core rspec-expectations rspec-mocks rspec-rails rspec-support].each do |lib|
-    gem lib, git: "https://github.com/rspec/#{lib}.git", branch: 'main'
-  end
+  gem 'rspec-rails'
+end
+
+group :test do
+  # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
+  gem 'capybara'
+  gem 'selenium-webdriver'
+  gem 'webdrivers'
+  gem 'rails-controller-testing'
 end
 
 group :development do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :request do
+
+  describe 'GET /index' do
+    before(:each) { get users_path }
+
+    it 'validates if action has correct response status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'validates if action rendered a correct template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'validates if correct text is displayed' do
+      expect(response.body).to include('Display All users')
+    end
+  end
+
+  describe 'GET /index' do
+    before(:each) { get users_path(10) }
+
+    it 'validates if action has correct response status' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'validates if action rendered a correct template' do
+      expect(response).to render_template('index')
+    end
+
+    it 'validates if correct text is displayed' do
+      expect(response.body).to include('Display All users')
+    end
+  end
+end
