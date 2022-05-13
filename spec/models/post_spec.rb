6b7adce044abc2d9ddb525927ca266094fe79c7c
sneeu-leeu <sa070197@gmail.com>
2022-05-13 15:18:41 +0200

    Add validation tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,35 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /posts' do
-    before(:each) { get '/users/:user_id/posts' }
+RSpec.describe Post, type: :model do
+  describe 'Post model tests' do
+    user = User.create(name: 'Danka', bio: 'Danka bio')
+    subject do
+      Post.new(title: 'Danka Post', text: 'This is Danka Post', user_id: user)
+    end
+    before { subject.save }
 
-    it 'validates if action has correct response status' do
-      expect(response).to have_http_status(:success)
+    it 'validates if post title is not blank' do
+      subject.title = nil
+      expect(subject).to_not be_valid
     end
 
-    it 'validates if action rendered a correct template' do
-      expect(response).to render_template('index')
+    it 'validates if the title has less than 250 characters' do
+      subject.title = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu'
+      expect(subject).to_not be_valid
     end
 
-    it 'validates if correct text is displayed' do
-      expect(response.body).to include('Display All User X Posts')
-    end
-  end
 
-  describe 'GET /show' do
-    before(:each) { get '/users/:user_id/posts/show' }
-
-    it 'validates if action has correct response status' do
-      expect(response).to have_http_status(:success)
+    it 'validates if likes counter is greater or equal than zero - right case' do
+      subject.likes_counter = 2
+      expect(subject).to_not be_valid
     end
 
-    it 'validates if action rendered a correct template' do
-      expect(response).to render_template('show')
+    it 'validates if likes counter is greater or equal than zero - wrong case' do
+      subject.likes_counter = -2
+      expect(subject).to_not be_valid
     end
 
-    it 'validates if correct text is displayed' do
-      expect(response.body).to include('Display Post X information')
+    it 'validates the 5 recent comments' do
+      expect(subject.recent_comments).to eq(subject.comments.last(5))
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :request do
-  describe 'GET /index' do
-    before(:each) { get users_path }
+RSpec.describe User, type: :model do
+  describe 'User model tests' do
+    subject { User.new(name: 'Rex', photo: 'Rex.png', bio: 'Rex bio', posts_counter: 0) }
+    before { subject.save }
 
-    it 'validates if action has correct response status' do
-      expect(response).to have_http_status(:success)
+    it 'validates if name is not blank' do
+      subject.name = nil
+      expect(subject).to_not be_valid
     end
 
-    it 'validates if action rendered a correct template' do
-      expect(response).to render_template('index')
+    it 'validates if posts_counter is an integer' do
+      subject.posts_counter = 'x'
+      expect(subject).to_not be_valid
     end
 
-    it 'validates if correct text is displayed' do
-      expect(response.body).to include('Display All users')
+    it 'validates if posts_counter is greater than or equal to zero' do
+      expect(subject.posts_counter).to be >= 0
     end
   end
 
-  describe 'GET /index' do
-    before(:each) { get users_path(10) }
-
-    it 'validates if action has correct response status' do
-      expect(response).to have_http_status(:success)
+  describe 'validate recent_posts method' do
+    before do
+      4.times do |post|
+        Post.create(user_id: subject, title: "This is a test post #{post}", text: 'This is a test text for test post')
+      end
     end
 
-    it 'validates if action rendered a correct template' do
-      expect(response).to render_template('index')
-    end
-
-    it 'validates if correct text is displayed' do
-      expect(response.body).to include('Display All users')
+    it 'validates three recent posts' do
+      expect(subject.recent_posts).to eq(subject.posts.last(3))
     end
   end
 end
