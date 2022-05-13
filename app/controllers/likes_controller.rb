df5e80359c9e10009feef36b4ac3efd4c3fec7b8
sneeu-leeu <sa070197@gmail.com>
2022-05-13 03:56:30 +0200

    Fix likes bug and add display create posts button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,7 +7,7 @@ class LikesController < ApplicationController
     )
     new_like.update_likes_counter
     if new_like.save
-      redirect_to "users/#{@post.user_id}/posts/#{@post.id}", notice: 'Liked!'
+      redirect_to "/users/#{@post.user_id}/posts/#{@post.id}", notice: 'Liked!'
     else
       redirect_to "/users/#{@post.user_id}/posts/#{@post.id}", alert: 'An error occured!'
     end
