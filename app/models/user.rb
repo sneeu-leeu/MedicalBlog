49d1bbbf01e9d029a625e6c4e01608155f381530
sneeu-leeu <sa070197@gmail.com>
2022-05-17 09:55:26 +0200

    Add delete button to posts view

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   has_many :posts, foreign_key: 'user_id'
-  has_many :comments, foreign_key: 'user_id'
-  has_many :likes, foreign_key: 'user_id'
+  has_many :comments, foreign_key: 'user_id', dependant: :destroy
+  has_many :likes, foreign_key: 'user_id', dependant: :destroy
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
